3563102783e0563a955a753014c84ca97c86f23e
Willem Wyndham <willem@ahalabs.dev>
2023-01-30 16:50:05 -0500

    feat: move logic to library

--- crates/wasm-xdr-tools-component/src/lib.rs
+++ crates/wasm-xdr-tools-component/src/lib.rs
@@ -1,5 +1,6 @@
-use soroban_env_host::xdr::{ReadXdr, ScObject, ScVal, WriteXdr, ScVec};
 use std::sync::Once;
+
+use strval::Spec;
 
 wit_bindgen_guest_rust::generate!("wasm-tools");
 
@@ -26,36 +27,14 @@ impl exports::Exports for WasmToolsJs {
         json_args: String,
     ) -> Result<Vec<u8>, String> {
         init();
-        let spec = strval::Spec::from_wasm(&wasm).map_err(|e| e.to_string())?;
-        let func = spec.find_function(&func_name).map_err(|e| e.to_string())?;
-        let value = serde_json::from_str::<serde_json::Value>(&json_args)
-            .map_err(|_| "failed to parse json".to_string())?;
-        let args = value.as_object().unwrap();
-        let mut encoded_args = func
-            .inputs
-            .iter()
-            .map(|input| {
-                let arg = args.get(&input.name.to_string_lossy()).unwrap();
-                spec.from_json(arg, &input.type_).map_err(|e| e.to_string())
-            })
-            .collect::<Result<Vec<_>, String>>()?;
-        let mut res = vec![
-            ScVal::Object(Some(ScObject::Bytes(
-                contract_id.as_bytes().try_into().unwrap(),
-            ))),
-            ScVal::Symbol(func_name.try_into().unwrap()),
-        ];
-        res.append(&mut encoded_args);
-            let sc_vec: ScVec = res.try_into().unwrap();
-        sc_vec.to_xdr().map_err(|e|e.to_string())
+        Spec::from_wasm(&wasm)
+            .and_then(|spec| spec.encode_args(&contract_id, &func_name, &json_args))
+            .map_err(|e| e.to_string())
     }
     fn decode_args(wasm: Vec<u8>, func_name: String, xdr_args: Vec<u8>) -> Result<String, String> {
         init();
-        let spec = strval::Spec::from_wasm(&wasm).map_err(|e| e.to_string())?;
-        let func = spec.find_function(&func_name).unwrap();
-        let args = ScVal::from_xdr(&xdr_args).unwrap();
-        spec.xdr_to_json(&args, &func.outputs[0])
-            .map(|v| v.to_string())
+        Spec::from_wasm(&wasm)
+            .and_then(|spec| spec.decode_args(&func_name, &xdr_args))
             .map_err(|e| e.to_string())
     }
 }
